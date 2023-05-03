eda4f8e7d7328e217c3bacf8187408f68e9ddb5a
John Shea <sheajohn@google.com>
2023-05-03 14:07:51 -0400

    Initial main_m3 branch commit

---
+++ app/src/main/java/com/example/dessertclicker/ui/theme/Color.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.dessertclicker.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val md_theme_light_primary = Color(0xFF006781)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_secondaryContainer = Color(0xFFCFE6F1)
+val md_theme_light_onSecondaryContainer = Color(0xFF071E26)
+val md_theme_light_background = Color(0xFFFBFCFE)
+
+val md_theme_dark_primary = Color(0xFF5FD4FD)
+val md_theme_dark_onPrimary = Color(0xFF003544)
+val md_theme_dark_secondaryContainer = Color(0xFF354A53)
+val md_theme_dark_onSecondaryContainer = Color(0xFFCFE6F1)
+val md_theme_dark_background = Color(0xFF191C1D)
