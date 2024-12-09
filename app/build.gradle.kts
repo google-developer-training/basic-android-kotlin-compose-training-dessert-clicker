bf7d9358ebf082fe1c4d4462ed57a7f228963685
Jose Alcérreca <jalc@google.com>
2024-12-09 13:47:49 +0000

    Sdk 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,12 +21,12 @@ plugins {
 
 android {
     namespace = "com.example.dessertclicker"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.dessertclicker"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
